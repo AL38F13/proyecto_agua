d5701174af10475fbe78e0434c5aae1e10a1b58c
Lanix 08 <Lanix 08>
2015-06-24 10:26:20 -0500

    doce
    doce

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnmenu').on('tap',function(){
+		navigatotor.vibrate(1000);
+	});
+}); 
+});
+
